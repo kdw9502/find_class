4329e454df2fd8952b6f0bb0ab52532fb1ef9b1b
kdw9502ACE <kdw9502@aceproject.com>
2018-02-23 18:18:40 +0900

    done

--- find_class.py
+++ find_class.py
@@ -2,19 +2,28 @@ import os
 import glob
 
 
-    
+depth=0    
 os.system("rm log.txt")
-files=glob.glob('*');
-for filename in files:
-    if filename.find('.aar')!=-1 or filename.find('.jar')!=-1:
-        os.system("unzip -d "+filename.split('.')[0]+" "+filename+">>log.txt");
-        fp_w=open('class_List_'+filename.split('.')[0]+'.txt','w')
+fp_w = None
+def inJarFindClass(filename,fp_w,depth):
+        depth+=1
+        os.system("unzip -d "+filename.split('.')[0]+" "+filename+">>"+"../"*depth+"log.txt");
+        if fp_w==None:
+            fp_w=open('class_List_'+filename.split('.')[0]+'.txt','w')
+        
         for path,directory,inFolderFiles in os.walk(''+filename.split('.')[0]):
             for inFolderFileName in inFolderFiles:
                 ext= inFolderFileName.split('.')[-1]
                 if ext == 'class':
                     fp_w.write(inFolderFileName+'\n')
-        fp_w.close()
+                elif ext == 'jar' or ext=='aar':
+                    inJarFindClass(filename.split('.')[0]+"/"+inFolderFileName,fp_w,depth)
         os.system("rm -rf "+filename.split('.')[0])
 
+
+
+files=glob.glob('*');
+for filename in files:
+    if filename.find('.aar')!=-1 or filename.find('.jar')!=-1:
+        inJarFindClass(filename,fp_w,depth)
 
