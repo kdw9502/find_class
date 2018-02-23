f36c843d51370f99f86d61d876759a6a2a77ae9d
kdw9502ACE <kdw9502@aceproject.com>
2018-02-23 17:53:51 +0900

    log 추가, 파일명 오류수정

--- find_class.py
+++ find_class.py
@@ -1,16 +1,17 @@
 import os
 import glob
 
-    
 
+    
+os.system("rm log.txt")
 files=glob.glob('*');
 for filename in files:
     if filename.find('.aar')!=-1 or filename.find('.jar')!=-1:
-        os.system("unzip -d "+filename.split('.')[0]+" "+filename);
+        os.system("unzip -d "+filename.split('.')[0]+" "+filename+">>log.txt");
         fp_w=open('class_List_'+filename.split('.')[0]+'.txt','w')
         for path,directory,inFolderFiles in os.walk(''+filename.split('.')[0]):
             for inFolderFileName in inFolderFiles:
-                ext= os.path.split('.')[-1]
+                ext= inFolderFileName.split('.')[-1]
                 if ext == 'class':
                     fp_w.write(inFolderFileName+'\n')
         fp_w.close()
