51394c22eff161446e44f019df9e9a3cc4ba691b
kdw9502ACE <kdw9502@aceproject.com>
2018-02-23 17:37:05 +0900

    first commit

---
+++ find_class.py
@@ -0,0 +1,18 @@
+import os
+import glob
+
+    
+
+files=glob.glob('*');
+for filename in files:
+    if filename.find('.aar')!=-1 or filename.find('.jar')!=-1:
+        os.system("unzip -d "+filename.split('.')[0]+" "+filename);
+        fp_w=open('class_List_'+filename.split('.')[0],'w')
+        for path,directory,inFolderFiles in os.walk(''+filename.split('.')[0]):
+            for inFolderFileName in inFolderFiles:
+                ext= os.path.split('.')[-1]
+                if ext == 'class':
+                    fp_w.write(inFolderFileName+'\n')
+        fp_w.close()
+
+
