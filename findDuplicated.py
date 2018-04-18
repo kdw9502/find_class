64ba9c2753d6131dccfea3a802b024e8abcc7ea0
kdw9502ACE <kdw9502@aceproject.com>
2018-04-18 18:21:13 +0900

    같은 파일의 jar 중 하나만 해제 후 검사하는 버그를 수정하였지만 프로그램이 암호문이 되었음, 회생불가수준

--- findDuplicated.py
+++ findDuplicated.py
@@ -1,6 +1,6 @@
 import os
 import filecmp
-
+import re
 
 def FindDuplicatedClass(foldername):
     if os.path.isdir(TEMP_DIRECTORY):
@@ -15,19 +15,24 @@ def FindDuplicatedClass(foldername):
 
 
 def FindOutDuplicatedClasses(classdict, jarLocateDict):
-    fp_w=open(DUPLE_LIST_OUTPUT_FILENAME, 'w')
+    fp_w=open(DUPLE_LIST_OUTPUT_FILENAME, 'w',encoding="utf-8")
+    # 클래스들의 이름, 해당 클래스가 존재하는 위치의 리스트
     for className,classPathList in classdict.items():
-        if len(classPathList) > 1 :
+        if len(classPathList) > 1 :# 중복이 존재할 경우 : 한 클래스가 여러곳에 존재할 경우
+            #형식출력
             PrintLongLine(fp_w)
+            #중복된 클래스
             print(className.replace(TEMP_DIRECTORY + '/', ''), file = fp_w)
             PrintLongLine(fp_w)
             print("duplicated at", file = fp_w)
             PrintLongLine(fp_w)
+            #중복 위치 출력
             for i in range(len(classPathList)):
-                print(str(i) + '. ' + classPathList[i].replace(TEMP_DIRECTORY + '/', ''), file = fp_w)
-                originPath = jarLocateDict.get(classPathList[i].split('/')[1] + '.aar', '') + jarLocateDict.get(classPathList[i].split('/')[1] + '.jar', '')
-                print( (' this class is in ' + originPath).encode('utf-8'), file = fp_w)
+                print(str(i) + '. ' + classPathList[i].replace(TEMP_DIRECTORY + '/', '').split('THJAR_')[1], file = fp_w)
+                originPath = jarLocateDict.get(classPathList[i].split('/')[1].split('THJAR_')[1] + '.aar', '') + jarLocateDict.get(classPathList[i].split('/')[1].split('THJAR_')[1] + '.jar', '')
+                print( (' this class is in ' + originPath), file = fp_w)
             PrintLongLine(fp_w)
+            #완전히 똑같은 클래스 출력
             for i in range(len(classPathList)-1):
                 for j in range(i + 1, len(classPathList)):
                     if filecmp.cmp( classPathList[i], classPathList[j]) :
@@ -60,29 +65,33 @@ def FindClasses(jardir, classdict):
 def UnzipJarAndFindClasses(classdict):
     unziplist = list()
     remainJar = True
+
     while remainJar :
         remainJar = False
         for path, directory, inFolderFiles in os.walk(TEMP_DIRECTORY):
             for inFolderFilename in inFolderFiles:
                 if inFolderFilename.endswith('.aar') or inFolderFilename.endswith('.jar'):
                     jarFilepath = path + '/' + inFolderFilename
-                    if jarFilepath not in unziplist:
-                        unziplist.append(jarFilepath)
+                    if re.sub('....THJAR_','',jarFilepath) not in unziplist:
+                        unziplist.append(re.sub('....THJAR_','',jarFilepath))
                         os.system("unzip -o -d " + jarFilepath.split('.aar')[0].split('.jar')[0]
                                   + ' ' + jarFilepath + '>>unziplog.txt')
-                        FindClasses(jarFilepath.split('.aar')[0].split('.jar')[0], classdict)
+                        #unzip 후 각 jar,aar의 압축해제 결과물이 저장된 디렉토리에서 Class들을 기록
+                        FindClasses(jarFilepath.split('.aar')[0].split('.jar')[0] , classdict)
                         remainJar = True
 
 
 def CollectJar(foldername, jarLocateDict):
     abspath = os.getcwd()
+    index = 0
     for path, directory, inFolderFiles in os.walk(foldername):
         for inFolderFilename in inFolderFiles:
             if inFolderFilename.endswith('aar') or inFolderFilename.endswith('jar'):
                 jarLocateDict[inFolderFilename] = path + '/' + inFolderFilename
-                os.system("cp " + path + "/" + inFolderFilename + " " + abspath + '/'+TEMP_DIRECTORY+'/'+inFolderFilename)
-
-
+                # 임시 폴더로 jar,aar파일 이동
+                os.system("cp " + path + "/" + inFolderFilename + " " + abspath
+                          + '/'+TEMP_DIRECTORY+'/' + "%04dTHJAR_"%index+inFolderFilename )
+                index = index +1
 def main():
     if os.path.exists("unziplog.txt"):
         os.system("rm unziplog.txt")
